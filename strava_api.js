f9d01588b60612f2aa234ccc549d0920f5798a60
a-stan004 <164003865+a-stan004@users.noreply.github.com>
2025-02-19 11:54:19 +0000

    API requests working
    - Tested with multiple accounts
    - User needs to create own account on Strava API for this to work
    - Currently using read all code as POST request
    - HTML page very simple
    - Now need to decrypt responses

--- strava_api.js
+++ strava_api.js
@@ -1,15 +1,17 @@
-client_id = 146431
-
-initial_url = `http://www.strava.com/oauth/authorize?client_id=${client_id}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=activity:read_all`
-
-
-const authlink = "https://www.strava.com/oauth/token?"
-
-function openInNewTab() {
-  window.open(initial_url, '_blank').focus();
+function getDetails (detail) {
+  const clientdetail = document.getElementById(detail).value;
+  return String(clientdetail)
 }
 
+function setReadAll() {
+  clientidurl = getDetails('clientid'),
+  readall_url = `http://www.strava.com/oauth/authorize?client_id=${clientidurl}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=activity:read_all`
+  window.open(readall_url);
+  document.getElementById('stageone').style.display = 'none';
+  document.getElementById('stagetwo').style.display = 'block';
+}
 
+const authlink = "https://www.strava.com/oauth/token?"
 
 function getActivities(res){
 
@@ -19,8 +21,13 @@ function getActivities(res){
 
 }
 
-
 function reAuthorise(){
+  const userclientid = clientidurl
+  console.log(userclientid)
+  const userclientsecret = getDetails('clientsecret')
+  console.log(userclientsecret)
+  const usercode = getDetails('code')
+  console.log(usercode)
   fetch(authlink,{
     method: 'post',
     headers: {
@@ -28,14 +35,29 @@ function reAuthorise(){
       'Content-Type': 'application/json'
     },
     body: JSON.stringify({
-      client_id : '146431',
-      client_secret : 'fa8f1dba14db96de096814962beaaee40779ebb6',
-      refresh_token : '63429733aca2481c8f5ea6037852e24c25cf93a6',
-      grant_type : 'refresh_token',
+      client_id : userclientid,
+      client_secret : userclientsecret,
+      code : usercode,
+      grant_type : 'authorization_code',
     })
   }).then(res => res.json())
       .then(res => getActivities(res))
+}
+
+function submitDetails(){
+      
+      reAuthorise()
 
 }
 
-reAuthorise()
+
+
+
+
+
+
+
+
+
+
+
