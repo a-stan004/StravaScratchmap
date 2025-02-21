9259f3e5f7dd45586f2b748e9a96c1148f084f3b
a-stan004 <164003865+a-stan004@users.noreply.github.com>
2025-02-21 17:13:20 +0000

    working API response and map

--- strava_api.js
+++ strava_api.js
@@ -20,7 +20,7 @@ let activitiesData = [];
 //Runs last and gets the activities
 function getActivities(res){
 
-  const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
+  const activities_link = `https://www.strava.com/api/v3/athlete/activities?per_page=200&access_token=${res.access_token}`
   fetch(activities_link)
     .then((res) => res.json())
     .then(data => parseActivities(data))
@@ -50,10 +50,45 @@ function reAuthorise(){
       .then(res => getActivities(res))
 }
 
+// Thanks to mgd722 for the github function to decrypt polylines, converted to JS
+function decodePolyline(polylineStr) {
+  let index = 0, lat = 0, lng = 0;
+  const coordinates = [];
+  const changes = { latitude: 0, longitude: 0 };
+
+  while (index < polylineStr.length) {
+      for (const unit of ['latitude', 'longitude']) {
+          let shift = 0, result = 0;
+
+          while (true) {
+              const byte = polylineStr.charCodeAt(index++) - 63;
+              result |= (byte & 0x1f) << shift;
+              shift += 5;
+              if (byte < 0x20) break;
+          }
+
+          changes[unit] = (result & 1) ? ~(result >> 1) : (result >> 1);
+      }
+
+      lat += changes.latitude;
+      lng += changes.longitude;
+
+      coordinates.push([lng / 1e5, lat / 1e5]);
+  }
+
+  return coordinates;
+}
+
+
 function parseActivities(data) {
-  let routes = [];
-  const original = JSON.parse(data)
-  
+  routes = [];
+    for (const item of data) {
+        polyline = item['map']['summary_polyline']
+        decoded_line = decodePolyline(polyline)
+        routes.push(decoded_line)
+        localStorage.setItem('scratchmap_routes', JSON.stringify(routes));
+    }
+  console.log(routes)
 }
 
 
