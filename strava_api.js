9c84271571605bd131cc54b0d3aa993d7abfa962
a-stan004 <164003865+a-stan004@users.noreply.github.com>
2025-02-21 10:26:09 +0000

    JSON response needs fixing

--- strava_api.js
+++ strava_api.js
@@ -1,8 +1,10 @@
+// Gets the value for an element by its id and converts it to a string
 function getDetails (detail) {
   const clientdetail = document.getElementById(detail).value;
   return String(clientdetail)
 }
 
+//Changes the users access to read all using client id
 function setReadAll() {
   clientidurl = getDetails('clientid'),
   readall_url = `http://www.strava.com/oauth/authorize?client_id=${clientidurl}&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=activity:read_all`
@@ -13,14 +15,18 @@ function setReadAll() {
 
 const authlink = "https://www.strava.com/oauth/token?"
 
+let activitiesData = [];
+
+//Runs last and gets the activities
 function getActivities(res){
 
   const activities_link = `https://www.strava.com/api/v3/athlete/activities?access_token=${res.access_token}`
   fetch(activities_link)
-      .then((res) => console.log(res.json()))
-
+    .then((res) => res.json())
+    .then(data => parseActivities(data))
 }
 
+//Authorises the user based off their details provided, uses the Medium method where authorization code used
 function reAuthorise(){
   const userclientid = clientidurl
   console.log(userclientid)
@@ -44,18 +50,17 @@ function reAuthorise(){
       .then(res => getActivities(res))
 }
 
-function submitDetails(){
-      
-      reAuthorise()
-
+function parseActivities(data) {
+  let routes = [];
+  const original = JSON.parse(data)
+  
 }
 
 
 
 
 
-
-
+console.log(activitiesData)
 
 
 
